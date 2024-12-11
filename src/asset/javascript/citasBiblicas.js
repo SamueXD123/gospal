90edb3d7718471cdbed6f08d6543c6f58b8be195
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-12-11 16:47:12 -0300

    fix de json

--- src/asset/javascript/citasBiblicas.js
+++ src/asset/javascript/citasBiblicas.js
@@ -3,150 +3,150 @@ export const citasBiblicas = [
         cita: "Jeremías 29:11",
         texto: "Porque yo sé los pensamientos que tengo acerca de vosotros, dice Jehová, pensamientos de paz, y no de mal, para daros el fin que esperáis.",
         parafraseo: "Dios tiene planes de paz y esperanza para nuestras vidas, asegurándonos un futuro lleno de propósito.",
-        imagen: "/asset/img/foto1.jpeg"
+        imagen: "/src/asset/img/foto1.jpeg"
     },
     {
         cita: "Romanos 8:28",
         texto: "Y sabemos que a los que aman a Dios, todas las cosas les ayudan a bien, esto es, a los que conforme a su propósito son llamados.",
         parafraseo: "Dios utiliza todo para nuestro bien cuando vivimos de acuerdo a su propósito. Él obra en todo para nuestro beneficio.",
-        imagen: "/asset/img/foto2.jpeg"
+        imagen: "/src/asset/img/foto2.jpeg"
     },
     {
         cita: "Hebreos 13:5",
         texto: "Sean vuestras costumbres sin avaricia, contentos con lo que tenéis ahora; porque él dijo: No te desampararé, ni te dejaré.",
         parafraseo: "Dios promete estar siempre con nosotros. Nunca nos dejará solos, sin importar las circunstancias.",
-        imagen: "/asset/img/foto3.jpeg"
+        imagen: "/src/asset/img/foto3.jpeg"
     },
     {
         cita: "Salmos 23:1",
         texto: "Jehová es mi pastor; nada me faltará.",
         parafraseo: "Dios es nuestro pastor, quien nos cuida y nos provee todo lo que necesitamos.",
-        imagen: "/asset/img/foto4.jpeg"
+        imagen: "/src/asset/img/foto4.jpeg"
     },
     {
         cita: "Isaías 41:10",
         texto: "No temas, porque yo estoy contigo; no desmayes, porque yo soy tu Dios que te esfuerzo; siempre te ayudaré, siempre te sustentaré con la diestra de mi justicia.",
         parafraseo: "No tenemos que temer porque Dios está con nosotros. Él nos da fuerzas y nos sostiene.",
-        imagen: "/asset/img/foto5.jpeg"
+        imagen: "/src/asset/img/foto5.jpeg"
     },
     {
         cita: "Mateo 11:28",
         texto: "Venid a mí todos los que estáis trabajados y cargados, y yo os haré descansar.",
         parafraseo: "Jesús nos invita a llevar nuestras cargas a Él para encontrar descanso y paz.",
-        imagen: "/asset/img/foto6.jpeg"
+        imagen: "/src/asset/img/foto6.jpeg"
     },
     {
         cita: "Filipenses 4:19",
         texto: "Mi Dios, pues, suplirá todo lo que os falta conforme a sus riquezas en gloria en Cristo Jesús.",
         parafraseo: "Dios suplirá todas nuestras necesidades según Su abundancia y poder.",
-        imagen: "/asset/img/foto7.jpeg"
+        imagen: "/src/asset/img/foto7.jpeg"
     },
     {
         cita: "Juan 14:27",
         texto: "La paz os dejo, mi paz os doy; yo no os la doy como el mundo la da. No se turbe vuestro corazón, ni tenga miedo.",
         parafraseo: "Jesús nos da una paz verdadera, que el mundo no puede ofrecer. No debemos temer ni preocuparnos.",
-        imagen: "/asset/img/foto8.jpeg"
+        imagen: "/src/asset/img/foto8.jpeg"
     },
     {
         cita: "Isaías 26:3",
         texto: "Tú guardarás en completa paz a aquel cuyo pensamiento en ti persevera; porque en ti ha confiado.",
         parafraseo: "Cuando confiamos en Dios, Él nos guarda en una paz perfecta.",
-        imagen: "/asset/img/foto9.jpeg"
+        imagen: "/src/asset/img/foto9.jpeg"
     },
     {
         cita: "2 Timoteo 1:7",
         texto: "Porque no nos ha dado Dios espíritu de cobardía, sino de poder, de amor y de dominio propio.",
         parafraseo: "Dios nos da un espíritu de valentía, poder, amor y autocontrol para vivir en Su propósito.",
-        imagen: "/asset/img/foto10.jpeg"
+        imagen: "/src/asset/img/foto10.jpeg"
     },
     {
         cita: "Juan 10:10",
         texto: "El ladrón no viene sino para hurtar y matar y destruir; yo he venido para que tengan vida, y para que la tengan en abundancia.",
         parafraseo: "Jesús vino a darnos una vida plena y abundante, lejos de la destrucción del enemigo.",
-        imagen: "/asset/img/foto11.jpeg"
+        imagen: "/src/asset/img/foto11.jpeg"
     },
     {
         cita: "Romanos 8:1",
         texto: "Ahora, pues, ninguna condenación hay para los que están en Cristo Jesús.",
         parafraseo: "En Cristo no somos condenados. Somos libres de la culpa y el pecado.",
-        imagen: "/asset/img/foto12.jpeg"
+        imagen: "/src/asset/img/foto12.jpeg"
     },
     {
         cita: "1 Pedro 5:7",
         texto: "Echando toda vuestra ansiedad sobre él, porque él tiene cuidado de vosotros.",
         parafraseo: "Podemos confiar en Dios y entregarle nuestras preocupaciones, porque Él cuida de nosotros.",
-        imagen: "/asset/img/foto13.jpeg"
+        imagen: "/src/asset/img/foto13.jpeg"
     },
     {
         cita: "Josué 1:9",
         texto: "Mira que te mando que te esfuerces y seas valiente; no temas ni desmayes, porque Jehová tu Dios estará contigo en dondequiera que vayas.",
         parafraseo: "Dios nos llama a ser valientes, porque Él siempre está con nosotros.",
-        imagen: "/asset/img/foto14.jpeg"
+        imagen: "/src/asset/img/foto14.jpeg"
     },
     {
         cita: "Santiago 1:5",
         texto: "Y si alguno de vosotros tiene falta de sabiduría, pídala a Dios, el cual da a todos abundantemente y sin reproche, y le será dada.",
         parafraseo: "Dios nos da sabiduría generosamente si la pedimos con fe.",
-        imagen: "/asset/img/foto15.jpeg"
+        imagen: "/src/asset/img/foto15.jpeg"
     },
     {
         cita: "Juan 16:33",
         texto: "Estas cosas os he hablado para que en mí tengáis paz. En el mundo tendréis aflicción; pero confiad, yo he vencido al mundo.",
         parafraseo: "Jesús nos da paz y esperanza en medio de las dificultades, porque Él ha vencido al mundo.",
-        imagen: "/asset/img/foto16.jpeg"
+        imagen: "/src/asset/img/foto16.jpeg"
     },
     {
         cita: "Proverbios 3:5-6",
         texto: "Fíate de Jehová de todo tu corazón, y no te apoyes en tu propia prudencia. Reconócelo en todos tus caminos, y él enderezará tus veredas.",
         parafraseo: "Cuando confiamos en Dios y lo ponemos en el centro, Él guía nuestro camino.",
-        imagen: "/asset/img/foto17.jpeg"
+        imagen: "/src/asset/img/foto17.jpeg"
     },
     {
         cita: "Isaías 40:31",
         texto: "Pero los que esperan a Jehová tendrán nuevas fuerzas; levantarán alas como las águilas; correrán, y no se cansarán; caminarán, y no se fatigarán.",
         parafraseo: "Al esperar en Dios, encontramos fuerzas renovadas para seguir adelante.",
-        imagen: "/asset/img/foto18.jpeg"
+        imagen: "/src/asset/img/foto18.jpeg"
     },
     {
         cita: "Efesios 2:8-9",
         texto: "Porque por gracia sois salvos por medio de la fe; y esto no de vosotros, pues es don de Dios; no por obras, para que nadie se gloríe.",
         parafraseo: "La salvación es un regalo de Dios, recibido por fe, y no por nuestras obras.",
-        imagen: "/asset/img/foto19.jpeg"
+        imagen: "/src/asset/img/foto19.jpeg"
     },
     {
         cita: "2 Corintios 12:9",
         texto: "Y me ha dicho: Bástate mi gracia; porque mi poder se perfecciona en la debilidad.",
         parafraseo: "La gracia de Dios es suficiente para nosotros. Su poder se manifiesta en nuestras debilidades.",
-        imagen: "/asset/img/foto20.jpeg"
+        imagen: "/src/asset/img/foto20.jpeg"
     },
     {
         cita: "Salmos 34:8",
         texto: "Gustad, y ved que es bueno Jehová; dichoso el hombre que confía en él.",
         parafraseo: "Podemos experimentar la bondad de Dios al confiar plenamente en Él.",
-        imagen: "/asset/img/foto21.jpeg"
+        imagen: "/src/asset/img/foto21.jpeg"
     },
     {
         cita: "Zacarías 4:6",
         texto: "No con ejército, ni con fuerza, sino con mi Espíritu, ha dicho Jehová de los ejércitos.",
         parafraseo: "La obra de Dios se realiza no por nuestra fuerza, sino por el poder de Su Espíritu.",
-        imagen: "/asset/img/foto22.jpeg"
+        imagen: "/src/asset/img/foto22.jpeg"
     },
     {
         cita: "1 Corintios 10:13",
         texto: "No os ha sobrevenido ninguna tentación que no sea humana; pero fiel es Dios, que no os dejará ser tentados más de lo que podéis resistir.",
         parafraseo: "Dios es fiel y nos da la fuerza para superar cualquier tentación.",
-        imagen: "/asset/img/foto23.jpeg"
+        imagen: "/src/asset/img/foto23.jpeg"
     },
     {
         cita: "Salmos 46:1",
         texto: "Dios es nuestro amparo y fortaleza, nuestro pronto auxilio en las tribulaciones.",
         parafraseo: "Dios es nuestro refugio y fortaleza, presente siempre en los momentos difíciles.",
-        imagen: "/asset/img/foto24.jpeg"
+        imagen: "/src/asset/img/foto24.jpeg"
     },
     {
         cita: "Deuteronomio 31:8",
         texto: "Y Jehová va delante de ti; él estará contigo, no te dejará, ni te desamparará; no temas ni te intimides.",
         parafraseo: "Dios camina con nosotros. Nunca nos abandona ni nos deja solos.",
-        imagen: "/asset/img/foto25.jpeg"
+        imagen: "/src/asset/img/foto25.jpeg"
     }
 ];
